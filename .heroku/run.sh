2a7963710086dd2cb4f8dbe9d86e65198584b95e
Vincent Diep <vincent.diep61@gmail.com>
2020-08-30 04:27:05 -0400

    Build script

---
+++ .heroku/run.sh
@@ -0,0 +1,2 @@
+openssl req -x509 -newkey rsa:4096 -sha256 -keyout "certs/key.pem" -out "certs/cert.pem" -nodes -days 365 -subj "/C=CA/ST=Ontario/L=Ottawa/O=Liquid Landscaping/CN=localhost"
+echo $IDP_CERT > certs/idp_cert.pem
